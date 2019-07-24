1a6fb5457a1c9ef99613f2ff23ce8dd996488756
DeGraciaMathieu <contact@degracia-mathieu.fr>
2019-07-24 22:58:51 +0200

    Add phpdoc

--- src/Crucible.php
+++ src/Crucible.php
@@ -7,11 +7,22 @@ use DeGraciaMathieu\LaravelManagerGenerator\Contracts\Template;
 
 class Crucible
 {
+    /**
+     * @var \DeGraciaMathieu\LaravelManagerGenerator\StubsService
+     */
+    protected $stubsService;
+
     public function __construct()
     {
         $this->stubsService = new StubsService;
     }
 
+    /**
+     * Makes template without saving
+     * @param  \DeGraciaMathieu\LaravelManagerGenerator\Contracts\Template   $template
+     * @param  \DeGraciaMathieu\LaravelManagerGenerator\Parameters $parameters
+     * @return \DeGraciaMathieu\LaravelManagerGenerator\Stub
+     */
     public function make(Template $template, Parameters $parameters) :Stub
     {
         $stub = $this->forge($template, $parameters);
@@ -19,6 +30,12 @@ class Crucible
         return $stub;
     }
 
+    /**
+     * Create a template and save this
+     * @param  \DeGraciaMathieu\LaravelManagerGenerator\Contracts\Template   $template
+     * @param  \DeGraciaMathieu\LaravelManagerGenerator\Parameters $parameters
+     * @return void
+     */
     public function create(Template $template, Parameters $parameters)
     {
         $stub = $this->forge($template, $parameters);
@@ -26,24 +43,36 @@ class Crucible
         $this->stubsService->save($stub, $this->getFullPath($template, $parameters));
     }
 
+    /**
+     * Create a template and save this
+     * @param  \DeGraciaMathieu\LaravelManagerGenerator\Contracts\Template   $template
+     * @param  \DeGraciaMathieu\LaravelManagerGenerator\Parameters $parameters
+     * @return \DeGraciaMathieu\LaravelManagerGenerator\Stub
+     */
     protected function forge(Template $template, Parameters $parameters) :Stub
     {
         $stub = $this->stubsService->load($template);
         
         $stub = $this->stubsService->hydrate($stub, $template->layers());
 
-        if (isset($template->properties['namespace'])) {
+        if (isset($template->namespace)) {
             $stub = $this->stubsService->hydrate($stub, $this->getNamespaceLayer($template, $parameters)); 
         }
 
         return $stub;
     }
 
+    /**
+     * Get a layers list to manage namespaces
+     * @param  \DeGraciaMathieu\LaravelManagerGenerator\Contracts\Template   $template
+     * @param  \DeGraciaMathieu\LaravelManagerGenerator\Parameters $parameters
+     * @return array
+     */
     protected function getNamespaceLayer(Template $template, Parameters $parameters) :array
     {
         $namespace = StringParser::concatenateForNamespace([
             $parameters->getBaseNamespace(),
-            $template->properties['namespace'],
+            $template->namespace,
         ]);
 
         return [
@@ -51,16 +80,28 @@ class Crucible
         ];
     }    
 
+    /**
+     * Get full template path
+     * @param  \DeGraciaMathieu\LaravelManagerGenerator\Contracts\Template   $template
+     * @param  \DeGraciaMathieu\LaravelManagerGenerator\Parameters $parameters
+     * @return string
+     */
     protected function getFullPath(Template $template, Parameters $parameters) :string
     {
         $path = $this->getPath($template, $parameters);
 
         return StringParser::concatenateForPath([
             $path,
-            $template->properties['path'],
+            $template->path,
         ]);
     }
 
+    /**
+     * Get template path
+     * @param  \DeGraciaMathieu\LaravelManagerGenerator\Contracts\Template   $template
+     * @param  \DeGraciaMathieu\LaravelManagerGenerator\Parameters $parameters
+     * @return string
+     */
     protected function getPath(Template $template, Parameters $parameters) :string
     {
         return StringParser::concatenateForPath([
