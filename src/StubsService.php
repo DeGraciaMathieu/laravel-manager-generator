05842f77a2d0835ae10e0e1d544b1bf42db78865
DeGraciaMathieu <contact@degracia-mathieu.fr>
2019-07-26 22:31:11 +0200

    phpdoc

--- src/StubsService.php
+++ src/StubsService.php
@@ -6,11 +6,24 @@ use DeGraciaMathieu\LaravelManagerGenerator\Contracts\Template;
 
 class StubsService
 {
+    /**
+     * @var $var \DeGraciaMathieu\LaravelManagerGenerator\PaintRollerService
+     */
+    protected $paintRollerService;
+
+    /**
+     * it's just the constructor
+     */
     public function __construct()
     {
         $this->paintRollerService = new PaintRollerService;
     }
 
+    /**
+     * Load a stub from a template
+     * @param  \DeGraciaMathieu\LaravelManagerGenerator\Contracts\Template
+     * @return \DeGraciaMathieu\LaravelManagerGenerator\Stub
+     */
     public function load(Template $template) :Stub
     {
         $content = file_get_contents(__DIR__ . '/stubs/' . $template->stub);
@@ -18,6 +31,12 @@ class StubsService
         return new Stub($content);
     }
 
+    /**
+     * Hydrate stub with layers
+     * @param \DeGraciaMathieu\LaravelManagerGenerator\Stub
+     * @param  array  $layers
+     * @return \DeGraciaMathieu\LaravelManagerGenerator\Stub
+     */
     public function hydrate(Stub $stub, array $layers) :Stub
     {
         $stub = $this->paintRollerService->brush($stub, $layers);
@@ -25,6 +44,12 @@ class StubsService
         return $stub;
     } 
 
+    /**
+     * [save description]
+     * @param \DeGraciaMathieu\LaravelManagerGenerator\Stub
+     * @param  string $fullPath [description]
+     * @return void
+     */
     public function save(Stub $stub, string $fullPath)
     {
         $fullPathWithExtension = StringParser::addFileExtension($fullPath);
